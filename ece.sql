cf58961f38b9ee3fd61c6f23c3126b87ab8be919
Sunitha415 <sunithasu7095@gmail.com>
2023-05-31 15:29:52 +0530

    firstcommitt

---
+++ ece.sql
@@ -0,0 +1,7 @@
+use ece;
+create table std1(
+id int primary key,
+name varchar(20) not null,
+dob date);
+insert into std1() values(1,"suni","2-11-2003");
+insert into std1(id,name) values(2,"radha");
